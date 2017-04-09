ae54a8878e4450ccc20c0b619fe42dc624c50aed
Andre Bering <andre@bering.in>
2017-04-10 00:07:47 +0200

    - added "Standard" module - https://github.com/feross/standard
    - fixed all things which Standard complained about

--- test/index.js
+++ test/index.js
@@ -1,39 +1,37 @@
-var assert = require('assert');
+/* global describe, it */
+var path = require('path')
+var assert = require('assert')
+var Handlebars = require('express-handlebars')
+var miscHelper = require(path.join(__dirname, '/../index.js'))
 
-var Handlebars = require('express-handlebars');
-var misc_helper = require(__dirname + '/../index.js');
+var hbs = Handlebars.create({})
+miscHelper.register(hbs.handlebars)
 
-var hbs = Handlebars.create({});
-// console.log(hbs);
-misc_helper.register( hbs.handlebars );
+describe('playTime', function () {
+  var tpl = hbs.handlebars.compile('{{playTime 72}}')
+  it('should return 01:12 for a value of 72', function () {
+    assert.equal(tpl(), '01:12')
+  })
 
-describe('playTime', function() {
-    var tpl = hbs.handlebars.compile('{{playTime 72}}');
-    it('should return 01:12 for a value of 72', function() {
-      assert.equal(tpl(), "01:12");
-    });
+  var tpl2 = hbs.handlebars.compile('{{playTime 636}}')
+  it('should return 10:36 for a value of 636', function () {
+    assert.equal(tpl2(), '10:36')
+  })
+})
 
-    var tpl2 = hbs.handlebars.compile('{{playTime 636}}');
-    it('should return 10:36 for a value of 636', function() {
-      assert.equal(tpl2(), "10:36");
-    });
-});
-describe( 'timeFromUnix helper', function(){
-	
-	var tpl = hbs.handlebars.compile('{{timeFromUnix null}}');
-    it('should return "---" when null value is provided', function() {
-	    assert.equal( tpl(), '---');
-    })
+describe('timeFromUnix helper', function () {
+  var tpl = hbs.handlebars.compile('{{timeFromUnix null}}')
+  it('should return "---" when null value is provided', function () {
+    assert.equal(tpl(), '---')
+  })
 
-    var tpl2 = hbs.handlebars.compile('{{timeFromUnix 1491773183}}');
-    it('should return "09.04.2017 23:26:23" when 1491773183 is provided', function() {
-	    assert.equal( tpl2(), '09.04.2017 23:26:23');
-    })
+  var tpl2 = hbs.handlebars.compile('{{timeFromUnix 1491773183}}')
+  it('should return "09.04.2017 23:26:23" when 1491773183 is provided', function () {
+    assert.equal(tpl2(), '09.04.2017 23:26:23')
+  })
 
-    var tpl3 = hbs.handlebars.compile('{{timeFromUnix 1491584400}}');
-    it('should return "07.04.2017 19:00:00" when 1491584400 is provided', function() {
-	    assert.equal( tpl3(), '07.04.2017 19:00:00');
-    })
-
-    
-});
+  var tpl3 = hbs.handlebars.compile('{{timeFromUnix 1491584400}}')
+  it('should return "07.04.2017 19:00:00" when 1491584400 is provided', function () {
+    assert.equal(tpl3(), '07.04.2017 19:00:00')
+  })
+})
